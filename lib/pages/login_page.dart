4f19151fd5fc948726ad2be45a4bdf8e862b3d00
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-04-09 10:19:12 +0530

    Day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // const LoginPage({Key? key}) : super(key: key);
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +18,9 @@ class LoginPage extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
+            SizedBox(
+              height: 20.0,
+            ),
             Image.asset(
               "assets/images/login_image.png",
               fit: BoxFit.cover,
@@ -19,7 +29,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -33,13 +43,17 @@ class LoginPage extends StatelessWidget {
                   const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
                 children: [
-                  TextField(
+                  TextFormField(
                     decoration: InputDecoration(
                       hintText: "Enter your User Name",
                       labelText: "User Name",
                     ),
+                    onChanged: (val) {
+                      name = val;
+                      setState(() {});
+                    },
                   ),
-                  TextField(
+                  TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
                       hintText: "Enter your Password",
@@ -49,13 +63,49 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                    onPressed: () {
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
+
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+
+                      await Future.delayed(Duration(milliseconds: 500));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                  )
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 500),
+                      width: changeButton ? 40 : 100,
+                      height: 40,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        borderRadius: changeButton
+                            ? BorderRadius.circular(25)
+                            : BorderRadius.circular(4),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
